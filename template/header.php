cc6247e3b40179cc68200757de66f915e0f97c67
vinicius_werlich <vinicius_werlich@SNSOO1EP01VM143.saojose.sc.senai.br>
2015-11-17 09:48:22 -0200

    criação do template

--- template/header.php
+++ template/header.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html>
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Loja VW</title>
-    <link rel="stylesheet" href="/css/foundation.css" />
+    <title><?php echo $title?></title>
+    <link rel="text/css" href="/template/style/app.css" />
     <link rel="stylesheet" href="/css/foundation-icons.css" />
     <link rel="stylesheet" href="/css/app.css" />
     <script src="/js/vendor/modernizr.js"></script>
